fd1d2e08e891865a10554e5a2882e79b54dfa912
Amrut <rxyamrutu@gmail.com>
2020-06-19 20:27:31 +0530

    added print value at given index and print index of given value

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,3 +1,7 @@
+//Linked list with 3 nodes
+//Printing the values of 3 nodes
+//Printing the value of node at given index
+//Printing the index of given value after searching
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -9,8 +13,27 @@ public:
 
 void printList(Node *n) {
 	while(n) { //n != NULL
-		cout << n->data << endl;
-		n = n->next;
+		cout << n -> data << endl;
+		n = n -> next;
+	}
+}
+
+void printValueAtIndex(Node *n, int index) {
+	while(index--) {
+		n = n -> next;
+	}
+	cout << n -> data << endl;
+}
+
+void searchNode(Node *n, int value) {
+	int count = 0;
+	while(n) {
+		if(value == n -> data) {
+			cout << "found at index " << count << endl;
+			return;
+		}
+		n = n -> next;
+		count++;
 	}
 }
 
@@ -23,14 +46,16 @@ int main() {
 	second = new Node();
 	third = new Node();
 
-	head->data = 4;
-	head->next = second;
+	head -> data = 1;
+	head -> next = second;
 
-	second->data = 2;
-	second->next = third;
+	second -> data = 2;
+	second -> next = third;
 
-	third->data = 6;
-	third->next = NULL;
+	third -> data = 3 ;
+	third -> next = NULL;
 
 	printList(head);
+	printValueAtIndex(head, 0);
+	searchNode(head, 1);
 }
